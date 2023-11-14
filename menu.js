377c928f30c3386626b05a8d13cfeb6d3fe057ba
Tomas Pereira <tomaspereira2k@gmail.com>
2023-11-14 14:58:20 -0300

    added functionality to the hamburger btn

--- menu.js
+++ menu.js
@@ -1,13 +1,39 @@
 const header = document.querySelector("header");
+const navbarContainer = document.querySelector(".navbar-container");
+const navbar = document.querySelector(".navbar");
+const mainContent = document.querySelector("main");
 
 const hamburgerBtn = document.createElement("img");
 hamburgerBtn.setAttribute("src", "./images/icon-menu.svg");
 
+const closeBtn = document.createElement("img")
+closeBtn.setAttribute("src", "./images/icon-close-menu.svg");
+closeBtn.setAttribute("class", "close-btn")
 
+if(window.innerWidth <= 425){
+    header.appendChild(hamburgerBtn)
+    navbar.style.flexDirection = "column";
+}
 
 window.addEventListener("resize", () => {
-    const width = window.innerWidth;
-    if(width <= 425){
+    if(window.innerWidth <= 425){
         header.appendChild(hamburgerBtn);
+        navbar.style.flexDirection = "column";
+    }else{
+        header.removeChild(hamburgerBtn);
+        navbar.style.flexDirection = "row";
+        navbarContainer.style.display = "flex";
     }
+})
+
+hamburgerBtn.addEventListener("click", () => {
+    navbarContainer.prepend(closeBtn)
+    navbarContainer.classList.toggle("mobile-navbar-container");
+    mainContent.style.filter = "blur(1px) opacity(0.5)";
+})
+
+closeBtn.addEventListener("click", () => {
+    navbarContainer.classList.toggle("mobile-navbar-container")
+    navbarContainer.removeChild(closeBtn);
+    mainContent.style.filter = "none";
 })
